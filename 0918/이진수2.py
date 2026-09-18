2b8effa2db71a9e2043f876d1d28851912874dc1
kanghyeonseo <hyunseo0613@naver.com>
2026-09-18 17:24:37 +0900

    수정

--- 0918/이진수2.py
+++ 0918/이진수2.py
@@ -20,4 +20,5 @@ for tc in range(1, T+1):
     if len(arr) > 12:
         print(f"#{tc} overflow")
     else:
-        print(f"#{tc}", *arr, sep="")
+        print(f"#{tc}", end=" ")
+        print(*arr, sep="")
